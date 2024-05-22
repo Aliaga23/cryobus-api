f168ca515446002689c05022dfe23fd575134ce4
cryobus <jaav2801@gmail.com>
2024-05-21 20:41:33 -0400

    Prueba 5

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const PORT = process.env.PORT || 3977;
 app.use(bodyParse.urlencoded({extended : true}));
 app.use(bodyParse.json());
 
-app.get("/",(req,res)=>{
-    const{username}=req.body;
-    res.status(200).send({msg :`Hola,${username}`});
 
-});
 app.listen(PORT,()=>{
     console.log(`Tu server esta listo en el puerto ${PORT}`);
 
